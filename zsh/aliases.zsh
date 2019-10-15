a72bfcc5dadb85a1e95bdf7289ed306ba1e769a9
Stephen Hanson <shanson@thoughtbot.com>
2019-10-15 09:39:28 -0500

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,6 +17,7 @@ alias mkdir="mkdir -p"
 alias e="$EDITOR"
 alias v="$VISUAL"
 alias path='print -l $PATH' # Pretty print the path
+alias fontnames="for file in "$arg"*.{ttf,otf}; do fc-scan --format "%{postscriptname}\n" $file; done"
 
 # alias "c {x}" to do "cd ~/dev/{x}"
 # tab completion set up in zsh/completion/_c
@@ -28,13 +29,15 @@ alias g-="git checkout -"
 alias ga="git add ."
 alias gac="git add . && git commit"
 alias gp="git push"
+alias gpf="git push --force-with-lease"
 alias gamend="git commit --amend --no-edit"
 alias gam="gamend"
 alias gaam="git add . && gamend"
 alias gb="git branch --sort=-committerdate --color | head"
+alias gdbak="git branch -D bak &>/dev/null"
+alias gbak="gdbak; git branch bak"
 alias gb20="gb -n 20"
 alias gbg="git branch --all --color --sort=-committerdate | grep -i"
-alias gcg="git log --all --grep"
 alias gc="git commit"
 alias gcf="git commit --fixup"
 alias gempty="git commit --allow-empty"
@@ -48,7 +51,8 @@ alias gDD="git branch -D"
 alias gl="git log -n 10"
 alias glg="git log -n 20 --grep"
 alias go="git checkout"
-alias gpr="git pull --rebase origin master"
+alias gpr="git pull --rebase"
+alias grom="git rebase origin/master"
 alias gri="git rebase -i"
 alias grc="git rebase --continue"
 alias gra="git rebase --abort"
@@ -59,8 +63,16 @@ alias guncommit="git reset --soft HEAD^"
 alias gunc="guncommit"
 alias gcomp="hub compare"
 alias gcompd="hub compare -b development"
+alias gpulls="hub browse -- pulls"
 alias gmv="git branch -m"
-alias gup='git branch --set-upstream-to=origin/$(git rev-parse --abbrev-ref HEAD) $(git rev-parse --abbrev-ref HEAD)'
+alias gcurrent="git rev-parse --abbrev-ref HEAD"
+alias gup='git branch --set-upstream-to=origin/$(gcurrent) $(gcurrent)'
+function co-pr() { git fetch origin pull/$1/head:pr/$1 && git checkout pr/$1; }
+
+# Usage, to rebase current branch off of other branch when other branch has
+# diverged: `gro other-branch last-stale-sha-or-branch-before-current-branch-changes`
+# Applies commits after the stale sha on top of `other-branch`
+function gro() { git rebase --onto $1 $2 $(gcurrent) }
 
 # == Heroku ====================================================================
 
@@ -70,11 +82,20 @@ alias hm="heroku run rake db:migrate --remote"
 
 # == Node ======================================================================
 
+alias ys="yarn start"
 alias yys="yarn && yarn start"
 alias ya="yarn add"
 alias yad="yarn add --dev"
 alias nunfuck="rm -rf node_modules && npm install"
 alias yunfuck="rm -rf node_modules && yarn install"
+alias yt="yarn test"
+alias ytp="yarn test --testPathPattern"
+
+# == React / React Native ======================================================
+
+alias rn="react-native"
+alias rni="react-native run-ios"
+alias rna="react-native run-android"
 
 # == Postgres ==================================================================
 
@@ -105,6 +126,7 @@ alias rdbr="rails db:rollback"
 alias rdbrt="rails db:rollback RAILS_ENV=test"
 alias rdbs="rails db:migrate:status | tail -n 15"
 alias be="bundle exec"
+alias rubo="git fetch && git diff-tree -r --no-commit-id --name-only master@\{u\} head | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop"
 
 # == tmux ======================================================================
 
@@ -118,4 +140,4 @@ alias tn="tmux new-session -d -s"
 
 # == vim =======================================================================
 
-alias retag='ctags -R --exclude=.gitignore --exclude=.git --exclude=log --exclude=tmp *'
+alias retag='ctags -R --exclude=.gitignore --exclude=.git --exclude=log --exclude=tmp --exclude=node_modules *'
